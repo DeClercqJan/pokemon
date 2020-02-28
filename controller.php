174de742d50dc5ab3186a67e4fb7cb44505eb94c
jan <declercqjan@gmail.com>
2020-02-28 18:42:57 +0100

    able to display pokes on the basis of search for type

--- controller.php
+++ controller.php
@@ -10,16 +10,16 @@ $pokemons_db = new Pokemons_DB;
 // GET DEFAULT LIST OF POKEMON
 $pokemons = $pokemons_db->show_pokemons();
 // var_dump_pretty($pokemons);
-foreach ($pokemons as $pokemon) {
-    echo '<pre>';
-    echo $pokemon->name;
-    echo '<pre>';
-    $pokemon_details =  $pokemons_db->get_pokemon_details($pokemon->name);
-    // var_dump_pretty($pokemon_details);
-    $pokemon_sprite = $pokemon_details->sprites->front_default;
-    // var_dump($pokemon_sprite);
-    echo "<img src=" . $pokemon_sprite . ">";
-}
+// foreach ($pokemons as $pokemon) {
+//     echo '<pre>';
+//     echo $pokemon->name;
+//     echo '<pre>';
+//     $pokemon_details =  $pokemons_db->get_pokemon_details($pokemon->name);
+//     // var_dump_pretty($pokemon_details);
+//     $pokemon_sprite = $pokemon_details->sprites->front_default;
+//     // var_dump($pokemon_sprite);
+//     echo "<img src=" . $pokemon_sprite . ">";
+// }
 
 // HOW TO GET DETAILS OF SPECIFIC POKEMON; ACCEPTS NAME AND ID NUMBER
 // $bulbasaur = $pokemons_db->get_pokemon_details("1");
@@ -34,3 +34,18 @@ foreach ($pokemons as $pokemon) {
 // $fire_pokemons = $pokemons_db->find_pokemons_by_type("fire");
 // $fire_pokemons = $pokemons_db->show_pokemons();
 // var_dump_pretty($fire_pokemons);
+if (isset($_POST["type"])) {
+    $type = $_POST["type"];
+    $pokemons = $pokemons_db->find_pokemons_by_type($type);
+    $pokemons = $pokemons_db->show_pokemons();
+    foreach ($pokemons as $pokemon) {
+        echo '<pre>';
+        echo $pokemon->pokemon->name;
+        echo '<pre>';
+        $pokemon_details =  $pokemons_db->get_pokemon_details($pokemon->pokemon->name);
+        // var_dump_pretty($pokemon_details);
+        $pokemon_sprite = $pokemon_details->sprites->front_default;
+        // var_dump($pokemon_sprite);
+        echo "<img src=" . $pokemon_sprite . ">";
+    }
+}
--- view.php
+++ view.php
@@ -1,11 +1,19 @@
-<?php declare(strict_types = 1);
-
-require_once("functions.php");
-require_once("controller.php");
-
-?>
-
-<form action="index.php" method="POST"></form>
-
 <?php
 
+declare(strict_types=1);
+
+require_once("functions.php");
+
+if (isset($_POST)) {
+    var_dump_pretty($_POST);
+}
+?>
+
+<form action="index.php" method="POST">
+    <label for="type">Enter type (NEEDS TO BE DROPDOWN)</label></br>
+    <input type="text" name="type" id="type"></input>
+    <input type="submit">
+</form>
+
+<?php
+require_once("controller.php");
