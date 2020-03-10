223121381d4cf97375488e62f13b5f0ca379d133
jan <declercqjan@gmail.com>
2020-03-10 17:15:24 +0100

    added form fields to favourtites card, but perhaps only one e-mailfield that takes an array of pokemon as input is better. Danny does this and while it is not the assignment, I think it's better

--- src/functions.php
+++ src/functions.php
@@ -23,7 +23,7 @@ function display_pokemons(array $pokemons, $pokemons_class, $favourites = false)
             $pokemon_name = $pokemon->get_pokemon_property("name"); ?>
             <!--            <div class="card" style="width: 18rem;">-->
             <!-- note: no bootstrap support for w-20, which would be better -->
-            <div class="card w-50 text-center border border-warning">
+            <div class="card w-25 text-center border border-warning">
                 <img class="card-img-top" src=<?php echo $pokemon->get_pokemon_property("image_url") ?> alt="Card
                      image cap">
                 <div class="card-body">
@@ -32,34 +32,18 @@ function display_pokemons(array $pokemons, $pokemons_class, $favourites = false)
                     <a href=<?php echo "'/overview.php?name=" . $pokemon_name . "'" ?> class="btn btn-secondary m-1">Specifications</a>
                     <!--                    <a href=-->
                     <?php //echo "'src/handle_cookie.php?name=" . $pokemon_name . "'"?><!-- class="btn btn-primary m-1">Add to favorite</a>-->
+                    <form action="src/handle_mail.php" method="POST">
+                        <label for='email'>Enter your email:</label></br>
+                        <input type='email' id='email' name='email'></br>
+                        <?php /*// sending data without creating input field
+                    */ ?>
+                        <input type='hidden' name="favourited_pokemon_to_mail"
+                               value=<?php /*echo $pokemon_details->name; */ ?>/>
+                        <input type="submit">
+                    </form>
                 </div>
             </div>
             <?php
-            /*            $favourites_old = unserialize($_COOKIE["favourites"]);
-                        foreach ($favourites_old as $favourite) {
-                            // adapted stuff from display_pokemons function function
-                            $pokemon_details = $pokemons_db->get_pokemon_details($favourite);
-                            echo '<pre>';
-                            echo $pokemon_details->name;
-                            echo '</pre>';
-                            $pokemon_sprite = $pokemon_details->sprites->front_default;
-                            echo "<img src=" . $pokemon_sprite . ">";
-                            // overview page
-                            $pokemon_id = $pokemon_details->id;
-                            echo "<a href='src/overview.php?id=$pokemon_id'>Specifications</a>";
-                            // echo "<a href='/handle_cookie.php?id=$pokemon_id'>Add to favorite</a>";
-                            */ ?><!--
-                <form action="src/handle_mail.php" method="POST">
-                    <label for='email'>Enter your email:</label>
-                    <input type='email' id='email' name='email'>
-                    <?php /*// sending data without creating input field
-                    */ ?>
-                    <input type='hidden' name="favourited_pokemon_to_mail" value=<?php /*echo $pokemon_details->name; */ ?>/>
-                    <?php
-            /*                    */ ?>
-                    <input type="submit">
-                </form>
-                --><?php
             /*            }*/
         }
     }// double if statement because the 2 can exist together. edit: no, not necessary as multiple calls are being made
