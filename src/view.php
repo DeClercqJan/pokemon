a32f093d5227cc2a7d855feebefa93966d6abd35
jan <declercqjan@gmail.com>
2020-03-13 12:38:07 +0100

    added e-mail validation error notifications

--- src/view.php
+++ src/view.php
@@ -53,16 +53,26 @@ require_once("controller.php");
         </div>
     </header>
     <main class="border border-primary row">
-        <!-- notification that e-mail has indeed been sent -->
-        <!--    <?php /*if (isset($_SESSION["mail_sent"]) && true == $_SESSION["mail_sent"]) {
-        */ ?>
-        <div class="border border-secondary col-12"> <?php
-        /*        echo "mail has been sent!";
-                // reset
-                $_SESSION["mail_sent"] = false;
-                    */ ?> </div> --><?php
-        /*    }
-            */ ?>
+        <!-- notification that e-mail has indeed been sent. probably should put this in separate component -->
+        <?php if (isset($_SESSION["mail_sent"]) && !isset($_SESSION["e_mail_error"])) {
+            ?>
+            <div class="col-12 alert alert-success" role="alert">
+                <?php
+                echo "mail has been sent";
+                unset($_SESSION["mail_sent"]);
+                ?>
+            </div>
+            <?php
+        } elseif (!isset($_SESSION["mail_sent"]) && isset($_SESSION["e_mail_error"])) {
+            ?>
+            <div class="col-12 alert alert-danger" role="alert">
+                <?php
+                echo $_SESSION["e_mail_error"];
+                echo "email error fires";
+                ?>
+            </div>
+            <?php
+        } ?>
         <div class="border border-secondary col-8 row d-flex justify-content-center">
             <?php
             // need to send current results page and such, definied in controller, to function as pagination component (included in display function) needs it
